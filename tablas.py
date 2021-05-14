cba9ba06ca3728d5c35e91c1ae304e7611bc20fb
kobando <80731419+kobando@users.noreply.github.com>
2021-05-14 12:13:13 -0500

    INFORME_07

---
+++ tablas.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import numpy as np
+#data = pd.read_excel(path)
+#df= pd.DataFrame(data, columns=[])
+
+#DATAFRAME USUARIOS
+dfu = pd.read_excel(r"C:\Users\valen\OneDrive\Escritorio\TABLA USUARIOS.xlsx")
+usuarios = pd.DataFrame(dfu, columns=['nombre','apellido','usuario','email','contrasena'])
+usuarios= usuarios.fillna('null')
+usuarios.set_index('nombre',inplace = True)
+
+#DATAFRAME FINCAS
+dff = pd.read_excel(r"C:\Users\valen\OneDrive\Escritorio\TABLA FINCAS.xlsx")
+fincas = pd.DataFrame(dff,columns=['idArea','finca'])
+fincas= fincas.fillna('null')
+fincas.set_index('idArea',inplace = True)
+
+#DATAFRAME OBSERVADORES
+dfo = pd.read_excel(r"C:\Users\valen\OneDrive\Escritorio\TABLA OBSERVADORES.xlsx")
+observadores = pd.DataFrame(dfo, columns=['primerNombre','segundoNombre','apellidos','celular','latitud','longitud'])
+observadores= observadores.fillna('null')
+observadores.set_index('primerNombre',inplace = True)
+
+#DATAFRAME VEREDAS
+dfv = pd.read_excel(r"C:\Users\valen\OneDrive\Escritorio\TABLA VEREDAS.xlsx")
+veredas = pd.DataFrame(dfv, columns=['departamento','ciudad','vereda'])
+veredas= veredas.fillna('null')
+veredas.set_index('departamento',inplace = True)
+
+#DATAFRAME REGISTROS
+dfr = pd.read_excel(r"C:\Users\valen\OneDrive\Escritorio\TABLA REGISTROS.xlsx")
+registros = pd.DataFrame(dfr, columns=['idFinca','fecha','precipitacion','temperaturaMaxima','temperaturaMinima'])
+#hacer que una columna sea el índice 
+# dataframe.set_index(Column_name,inplace = True)
+registros.set_index('idFinca',inplace = True)
+registros= registros.fillna('null')
