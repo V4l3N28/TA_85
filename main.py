2b1b9bd4f86e04a47fd2ee954c1bd3042293122f
Valentina Ortega Paz <80727758+V4l3N28@users.noreply.github.com>
2021-06-04 20:43:15 -0500

    .
    Co-Authored-By: Isaac Mesa Maya <80772803+Isaacmesa92@users.noreply.github.com>
    Co-Authored-By: kobando <80731419+kobando@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ import sqlite3,os,functools
 from functools import wraps
 import pandas as pd
 import numpy as np
-
+import random
 
 app = Flask(
   __name__,
   template_folder='templates')
 app.secret_key = os.urandom( 24 )
 
+
 def login_required(view):
     @functools.wraps( view )
     def wrapped_view(*args,**kwargs):
         if g.user is None:
-            #flash('Primero necesitas iniciar sesión')
-            return redirect( url_for('iniciosesion'))
-        return view( *args, **kwargs )
+            flash('Primero necesitas iniciar sesión')
+            return redirect( '/IniciarSesion/' )
+        return view( **kwargs )
     return wrapped_view
+
 #Se define una funcion get_db() en donde se utiliza un bloque try except el cual captura un error en el caso de que suceda
 #Dentro de try abrimos un condicional if en donde verificamos si una variable 'db' no está en g (variables globales)
 # si 'db' no está en g, entonces nos conectamos a la base de datos y le asignamos db a las variables globales
+#CONEXION BASE DATOS
 def get_db():
     try:
         if 'db' not in g:
@@ -38,7 +41,6 @@ def close_db():
     if db is not None:
         db.close()
 
-
 #Se define una funcion load_logged_in_user() en la cual se obtiene un user_id de la sesion y se guarda en una variable para trabajarlo más comodamente
 #Si el usuario de la sesion (user_id) está vacío, se le asigna a None a la variable global de user
 #En el caso contrario, si el usuario de la sesión no está vacío, se obtiene ese dato de la base de datos y se le asigna la variable global
@@ -53,7 +55,7 @@ def load_logged_in_user():
             'SELECT * FROM usuarios WHERE id = ?', (user_id,)
         ).fetchone()
 
-
+#DATAFRAME DATOS DE BASE DE DATOS
 def dataframe():
   #Se hace la conexión a la base de datos bases_07.db por medio de la asignación a una variable conn
   conn=sqlite3.connect('bases_04.db')
@@ -62,8 +64,8 @@ def dataframe():
   #En este punto, se corre el programa para la creación de la base de datos y se comentan las líneas de creación de tablas (debido a que si se dejan, el programa lanzará un mensaje diciendo que las tablas ya fueron creadas anteriormente)
   #Se define una variable df como el Dataframe del contenido de la variable cursor y se define el nombre de sus columnas
   df = pd.DataFrame(cursor, columns=['fecha','precipitacion','temperaturaMaxima','temperaturaMinima'])
-  
   return df
+
 #Dentro de la conexion se convierten las tablas importadas de la hoja tablas.py a tablas de sql
 #veredas.to_sql('veredas', conn)
 #observadores.to_sql('dfo', conn)
@@ -71,21 +73,21 @@ def dataframe():
 #registros.to_sql('registros', conn)
 #usuarios.to_sql('usuarios', conn)
 
+#FUNCIONES DATOS CLIMÁTICOS
 #Se procede a la creacion de varias funciones como sumatoria, promedio, valor máximo y valor mínimo del filtro de datos en columnas específicas, cada funcion pide  dos cosas; un año y un dataframe
 def sumatoria_prec(year,df):
   conn=sqlite3.connect('bases_04.db')
   #Se crea un cursor 
-  cursor = conn.cursor() 
+  cursor = conn.cursor()
   #En la línea 31 se utiliza la siguiente estructura (línea 27) para obtener la sumatoria de la precipitacion por mes de un año en específico
   #cursor.execute("SELECT strftime('%m', date) as valmonth, SUM(column) FROM registros WHERE strftime('%Y', date)='year_number' GROUP BY valmonth")
   #En la estructura anterior se hace una seleccion (por medio de un SELECT) de los meses dentro de una columna (asignandole un nombre por medio de 'as' un nombre 'month') y de la sumatoria de otra columna(por medio de la funcion SUM()) 
   #Estos datos se recolectan de una tabla de datos (por medio de FROM) y se utiliza un filtro de datos (WHERE) en el que se pide que esos datos estén dentro de un valor de año dado por la variable year. 
   #Además, se agrupan estos datos por meses (por medio de GROUP BY month).
+
   cursor.execute("SELECT strftime('%m', fecha) as month, SUM(precipitacion) FROM registros WHERE strftime('%Y', fecha)=? GROUP BY month",[year])
   #Por medio de un fetchall() se recolectan los datos filtrados del cursor
   filtered_db= cursor.fetchall()
-  print(filtered_db)
-  print(type(filtered_db))
   #Estos datos se convierten a Dataframe con nombre de columnas especificados
   db_to_dataframe= pd.DataFrame(filtered_db,columns=['fecha','precipitacion'])
   #Se retorna el dataframe
@@ -95,17 +97,14 @@ def sumatoria_prec(year,df):
 #(No se utiliza GROUP BY month para que la funcion solo nos arroje un resultado general y no por partes)
 def promedio_prec(year,df):
     conn=sqlite3.connect('bases_04.db')
-    #Se crea un cursor 
     cursor = conn.cursor() 
     cursor.execute("SELECT strftime('%m', fecha) as month, AVG(precipitacion) FROM registros WHERE strftime('%Y', fecha)=?",[year])
     filtered_db= cursor.fetchall()
-    
     db_to_dataframe= pd.DataFrame(filtered_db,columns=['fecha','precipitacion'])
     return db_to_dataframe
 
 def temp_max(year,df):
     conn=sqlite3.connect('bases_04.db')
-    #Se crea un cursor 
     cursor = conn.cursor() 
     cursor.execute("SELECT strftime('%m', fecha) as month, MAX(temperaturaMaxima) FROM registros WHERE strftime('%Y', fecha)=?",[year])
     filtered_db= cursor.fetchall()
@@ -114,15 +113,11 @@ def temp_max(year,df):
 
 def temp_min(year,df):
     conn=sqlite3.connect('bases_04.db')
-    #Se crea un cursor 
     cursor = conn.cursor() 
     cursor.execute("SELECT strftime('%m', fecha) as month, MIN(temperaturaMinima) FROM registros WHERE strftime('%Y', fecha)=?",[year])
     filtered_db= cursor.fetchall()
     db_to_dataframe= pd.DataFrame(filtered_db,columns=['fecha','temperaturaMinima'])
     return db_to_dataframe
-
-
-
 
 #este .py tiene la finalidad de mapear cada uno de los links con su respectiva funcion
 #Conexion a \templates\HOME la cual seria establecida como la pagina principal
@@ -131,6 +126,7 @@ def temp_min(year,df):
 '''app.route(rule, options)
     El parámetro de regla (rule) representa el enlace de URL con la función.
     Las opciones (options) son una lista de parámetros que se enviarán al objeto Regla subyacente.'''
+
 @app.route('/')
 def HOME():
   return render_template("HOME.html")
@@ -139,7 +135,7 @@ def HOME():
 @app.route('/IniciarSesion/', methods=('GET','POST'))
 def iniciosesion():
   if g.user:
-    return redirect( '/General')
+    return redirect( '/General/')
   if request.method == 'POST':
     usuario = request.form['usuario']
     contrasena = request.form['contrasena']
@@ -149,7 +145,7 @@ def iniciosesion():
     if user is None or contraseña_bd is None:
       message = 'Usuario o contraseña Incorrectos'
       flash(message)
-      return redirect('/IniciarSesion')
+      return redirect('/IniciarSesion/')
     else:
       contraseña_b=contraseña_bd[0]
     if contrasena==contraseña_b:
@@ -159,10 +155,10 @@ def iniciosesion():
       return  redirect('/General')
     else:
       flash('Contraseña incorrecta')
-      return redirect( '/IniciarSesion')
+      return redirect('/IniciarSesion/')
   return render_template("ventanaInicioSESION.html")
 
-##Conexion a \templates\entanvaRegistroUSUARIO
+##Conexion a \templates\ventanaRegistroUSUARIO
 @app.route('/Registrarse/', methods=('GET','POST'))
 #Se define una función ventanaRegistroUSUARIO(), la cual verifica si el usuario ya está registrado y lo envía al home
 #Se abre un bloque try except en donde se toman los datos del formulario (request.form), se verifica que el correo y las contraseñas sean iguales
@@ -183,7 +179,7 @@ def ventanaRegistroUSUARIO():
         db = get_db()
         db.execute("INSERT INTO usuarios(nombre, apellido, usuario, email, contrasena) VALUES ('%s','%s','%s','%s','%s')" % (nombre, apellido, usuario, correo, contrasena))
         db.commit()
-        return redirect('/IniciarSesion')
+        return redirect('/IniciarSesion/')
       else:
         flash('Correo o contraseñas no coinciden')
         return render_template("ventanaRegistroUSUARIO.html", nombre = nombre, Apellido = apellido, Usuario=usuario)
@@ -249,18 +245,17 @@ def CerrarSesion():
   if g.user is None:
     flash('Has cerrado sesión satisfactoriamente')
     return redirect('/')
-  return redirect( '/IniciarSesion' )
+  return redirect( '/IniciarSesion/' )
   
-    
-#El constructor de  toma el nombre del módulo actual (__name__) como argumento.
+#El constructor de  toma el nombre del módulo actual (__name__) como argumento.    
 if __name__ == "__main__":
-      #El método run () de la clase Flask ejecuta la aplicación en el servidor de desarrollo local.
+        #El método run () de la clase Flask ejecuta la aplicación en el servidor de desarrollo local.
   #app.run(host, port, debug, options) - todos los parametros son opcionales
 
   ''' host (anfitrion) - El valor predeterminado es 127.0.0.1 (localhost). Configure en "0.0.0.0" para que el servidor esté disponible externamente
       port(puerto) - El valor predeterminado es 5000
       debug - El valor predeterminado es falso. Si se establece en verdadero, proporciona información del debug.
       options(opciones) - Para ser reenviado al servidor de herramientas subyacente.'''
-  
-  app.run( host='0.0.0.0', debug=True, port=8080)
+
+  app.run( host='0.0.0.0', debug=True, port=random.randint(2000, 9000) )
   
